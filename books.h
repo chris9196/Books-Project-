77ed90a94e5c58179bf0f45c5cb3d50e5c435ea8
chris9196 <60952891+chris9196@users.noreply.github.com>
2020-02-27 19:17:48 -0800

    Update books.h

--- books.h
+++ books.h
@@ -1,10 +1,26 @@
 #pragma once
 // Books object
-class Books
+class Book
 {
-private:
-  
-
-
-
-};
+    public:
+    string title, isbn, author, publisher,
+    dateAdded, quantity, wholeSale, retailSale;
+    
+    void setTitle(string);
+    void setISBN(string);
+    void setAuthor(string);
+    void setPublisher(string);
+    void setDateAdded(string);
+    void setQuantity(string);
+    void setWholeSale(string);
+    void setRetailSale(string);
+    
+    string getTitle();
+    string getISBN();
+    string getAuthor();
+    string getPublisher();
+    string getDateAdded();
+    string getQuantity();
+    string getWholeSale();
+    string getRetailSale();
+}
