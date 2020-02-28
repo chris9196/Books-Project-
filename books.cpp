d8c81425dce9647f6d64b2d20391c7b62996230a
chris9196 <60952891+chris9196@users.noreply.github.com>
2020-02-27 19:25:43 -0800

    Update books.cpp

--- books.cpp
+++ books.cpp
@@ -1,2 +1,26 @@
 #include "books.h"
 
+class Book
+{
+    public:
+    string title, isbn, author, publisher,
+    dateAdded, quantity, wholeSale, retailSale;
+    
+    void setTitle(string a) {title = a;}
+    void setISBN(string b) {isbn = b;}
+    void setAuthor(string c) {author = c;}
+    void setPublisher(string d) {publisher = d;}
+    void setDateAdded(string e) {dateAdded = e;}
+    void setQuantity(string f) {quantity = f;}
+    void setWholeSale(string g) {wholeSale = g;}
+    void setRetailSale(string h) {retailSale = h;}
+    
+    string getTitle() {return title;}
+    string getISBN() {return isbn;}
+    string getAuthor() {return author;}
+    string getPublisher() {return publisher;}
+    string getDateAdded() {return dateAdded;}
+    string getQuantity() {return quantity;}
+    string getWholeSale() {return wholeSale;}
+    string getRetailSale() {return retailSale;}
+}
